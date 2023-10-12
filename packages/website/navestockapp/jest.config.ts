93621d0e5bd1523269554802617524c4d652293e
Lefras Coetzee <lefras.coetzee@gmail.com>
2023-10-12 10:25:15 +0100

    Moved Navestock-App to new folder

--- packages/website/navestockapp/jest.config.ts
+++ packages/website/navestockapp/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'website-website',
+  displayName: 'website-navestockapp',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../coverage/packages/website/website',
+  coverageDirectory: '../../../coverage/packages/website/navestockapp',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
