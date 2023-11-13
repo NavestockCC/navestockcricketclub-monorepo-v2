da521047a35ec41163890690f0c581261c2f47ca
Lefras Coetzee <lefras.coetzee@gmail.com>
2023-11-13 17:40:45 +0000

    Sponsors Component Created

---
+++ libs/website/ncc-sponsors/README.md
@@ -0,0 +1,7 @@
+# sponsors
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test sponsors` to execute the unit tests.
---
+++ libs/website/ncc-sponsors/src/lib/sponsors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ncc-website-sponsors',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sponsors.component.html',
+  styleUrls: ['./sponsors.component.scss'],
+})
+export class SponsorsComponent {}
