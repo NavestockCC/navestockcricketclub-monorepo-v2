d923256e109d64678e41b31908b21a417054936f
Lefras Coetzee <lefras.coetzee@gmail.com>
2023-12-22 21:22:19 +0000

    vertical-card generated

---
+++ libs/website/vertical-card/src/lib/vertical-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ncc-website-vertical-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './vertical-card.component.html',
+  styleUrls: ['./vertical-card.component.scss'],
+})
+export class VerticalCardComponent {}
--- packages/website/navestockapp/src/app/app.component.ts
+++ packages/website/navestockapp/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SponsorsComponent } from '@navestockcricketclub-monorepo-v2/sponsors';
+import {VerticalCardComponent} from '@ncc/component-verticalCard';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, SponsorsComponent],
+  imports: [
+    RouterModule, 
+    SponsorsComponent,
+    VerticalCardComponent
+  ],
   selector: 'ncc-app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
