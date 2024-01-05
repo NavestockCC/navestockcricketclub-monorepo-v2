36212c1867a6c65b07689aff55bf181ee30b3030
Lefras Coetzee <lefras.coetzee@gmail.com>
2024-01-05 12:58:42 +0000

    Recruitment Banned created

--- packages/website/navestockapp/src/app/app.component.ts
+++ packages/website/navestockapp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 import { SponsorsComponent } from '@navestockcricketclub-monorepo-v2/sponsors';
 import {WelcomeCardComponent} from './welcome-card/welcome-card.component';
 import {FindUsCardComponent} from './FindUs-card/find-us-card.component';
+import { RecruitmentComponent } from './recruitment/recruitment.component';
 
 @Component({
   standalone: true,
@@ -10,7 +11,8 @@ import {FindUsCardComponent} from './FindUs-card/find-us-card.component';
     RouterModule, 
     SponsorsComponent,
     WelcomeCardComponent,
-    FindUsCardComponent
+    FindUsCardComponent,
+    RecruitmentComponent
   ],
   selector: 'ncc-app-root',
   templateUrl: 'app.component.html',
---
+++ packages/website/navestockapp/src/app/recruitment/recruitment.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {  } from '@navestockcricketclub-monorepo-v2/ui-img';
+
+@Component({
+  selector: 'ncc-app-recruitment',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recruitment.component.html',
+  styleUrls: ['./recruitment.component.scss'],
+})
+export class RecruitmentComponent {
+
+}
